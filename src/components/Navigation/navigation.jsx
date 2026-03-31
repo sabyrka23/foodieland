a96ea716fbed5edffb8c454991a86e4fd55582a0
Sabyrka <sabyrka23@gmail.com>
2026-04-01 01:44:16 +0500

    feat: implement navigation component with menu items

---
+++ src/components/Navigation/navigation.jsx
@@ -0,0 +1,24 @@
+import './Navigation.scss'
+import clsx from 'clsx'
+import { menuItems } from './navigation.data'
+
+export default (props) => {
+  const { url, className } = props
+
+  return (
+    <nav className={clsx('menu', className)}>
+      <ul className="menu__list">
+        {menuItems.map(({ label, href }) => (
+          <li className="menu__item" key={href}>
+            <a
+              className={clsx('menu__link', href === url && 'is-active')}
+              href={href}
+            >
+              {label}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
