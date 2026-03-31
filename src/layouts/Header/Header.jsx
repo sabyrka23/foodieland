53f9f23ae55cf97c6eb08f487c43e17f4a3a2c59
Sabyrka <sabyrka23@gmail.com>
2026-04-01 01:57:50 +0500

    feat: add BurgerButton component and enhance Navigation with extra attributes

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -2,16 +2,29 @@ import './Header.scss'
 import Logo from '@/components/Logo'
 import Navigation from '@/components/Navigation'
 import Socials from '@/components/Socials'
+import BurgerButton from '@/components/BurgerButton'
 
 export default (props) => {
   const { url } = props
 
   return (
-    <header className="header" data-js-overlay-menu="">
+    <header className="header" data-js-mobile-menu="">
       <div className="header__inner container">
         <Logo className="header__logo" loading="eager" />
-        <Navigation className="header__menu" url={url} />
+        <Navigation
+          className="header__menu"
+          url={url}
+          extraAttrs={{
+            'data-js-mobile-menu-overlay': '',
+          }}
+        />
         <Socials className="header__soc1als" />
+        <BurgerButton
+          className="header__burger-button"
+          extraAttrs={{
+            'data-js-mobile-menu-burger-button': '',
+          }}
+        />
       </div>
     </header>
   )
