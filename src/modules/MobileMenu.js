bcf4e8adfebb26b3d08e856dd55fd89cdf7e1fd5
Sabyrka <sabyrka23@gmail.com>
2026-04-01 01:57:58 +0500

    feat: add Header class for mobile menu functionality

---
+++ src/modules/MobileMenu.js
@@ -0,0 +1,33 @@
+class Header {
+  selectors = {
+    root: '[data-js-mobile-menu]',
+    overlay: '[data-js-mobile-menu-overlay]',
+    burgerButton: '[data-js-mobile-menu-burger-button',
+  }
+
+  stateClasses = {
+    isActive: 'is-active',
+    isLock: 'is-lock',
+  }
+
+  constructor() {
+    this.rootElement = document.querySelector(this.selectors.root)
+    this.overlayElement = this.rootElement.querySelector(this.selectors.overlay)
+    this.burgerButtonElement = this.rootElement.querySelector(
+      this.selectors.burgerButton
+    )
+    this.bindEvents()
+  }
+
+  onBurgerButtonClick = () => {
+    this.overlayElement.classList.toggle(this.stateClasses.isActive)
+    this.burgerButtonElement.classList.toggle(this.stateClasses.isActive)
+    document.documentElement.classList.toggle(this.stateClasses.isLock)
+  }
+
+  bindEvents() {
+    this.burgerButtonElement.addEventListener('click', this.onBurgerButtonClick)
+  }
+}
+
+export default Header
