b8db9a91c6d7cee987bb95b1c798b1e3b17fd625
Sabyrka <sabyrka23@gmail.com>
2026-04-02 01:07:33 +0500

    feat: add InstagramView component to index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import Categories from '@/sections/Categories'
 import Recipes from '@/sections/Recipes'
 import SubscribeForm from '@/sections/SubscribeForm'
 import Banner from '@/sections/Banner'
+import InstagramView from '@/sections/InstagramView'
 
 export const metadata = {
   title: 'Home',
@@ -15,6 +16,7 @@ export default () => {
       <Categories />
       <Recipes />
       <Banner />
+      <InstagramView />
       <SubscribeForm />
     </>
   )
