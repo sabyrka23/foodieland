e41cbe5d3d1fbec40a9e83f0131aea5a4d98a050
Sabyrka <sabyrka23@gmail.com>
2026-04-01 18:20:36 +0500

    feat: enhance Footer component with structured layout and additional elements

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,9 +1,26 @@
 import './Footer.scss'
+import Navigation from '@/components/Navigation'
+import Socials from '@/components/Socials'
+import Logo from '@/components/Logo'
 
 export default () => {
   return (
     <footer className="footer">
-      <div className="footer__inner container">FOOTER</div>
+      <div className="footer__inner container">
+        <div className="footer__body">
+          <div className="footer__body-info">
+            <Logo className="footer__logo" />
+            <div className="footer__subtitle">
+              Lorem ipsum dolor sit amet, consectetuipisicing elit
+            </div>
+          </div>
+          <Navigation className="footer__menu" />
+        </div>
+        <div className="footer__extra">
+          <div className="footer__copyright">© 2020. Portfolio project.</div>
+          <Socials className="footer__soc1als" />
+        </div>
+      </div>
     </footer>
   )
 }
