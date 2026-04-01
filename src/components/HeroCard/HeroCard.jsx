f827e94fe05e8935d02fb694b7d392312ad568e6
Sabyrka <sabyrka23@gmail.com>
2026-04-01 14:50:20 +0500

    feat: restructure HeroCard component for improved layout and styling

--- src/components/HeroCard/HeroCard.jsx
+++ src/components/HeroCard/HeroCard.jsx
@@ -19,41 +19,45 @@ export default (props) => {
   return (
     <article className={clsx('hero-card', className)}>
       <div className="hero-card__body">
-        <div className="hero-card__category">
-          <Image src={category.imgSrc} />
-          <span>{category.label}</span>
-        </div>
-        <h2 className="hero-card__title h1">{title}</h2>
-        <div className="hero-card__description">
-          <p>{description}</p>
-        </div>
-        <div className="hero-card__tags">
-          <ul className="hero-card__tags-list">
-            {tags.map(({ icon, label }) => (
-              <li className="hero-card__tags-item" key={label}>
-                <Icon name={icon} hasFill />
-                <span>{label}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="hero-card__author">
-          <Image src={author.imgSrc} />
-          <div className="hero-card__author-body">
-            <h3 className="hero-card__author-name">{author.name}</h3>
-            <time
-              className="hero-card__author-date"
-              dateTime={author.date.dateTime}
-            >
-              {author.date.label}
-            </time>
+        <div className="hero-card__body-main">
+          <div className="hero-card__category">
+            <Image src={category.imgSrc} />
+            <span>{category.label}</span>
+          </div>
+          <h2 className="hero-card__title h1">{title}</h2>
+          <div className="hero-card__description">
+            <p>{description}</p>
+          </div>
+          <div className="hero-card__tags">
+            <ul className="hero-card__tags-list">
+              {tags.map(({ icon, label }) => (
+                <li className="hero-card__tags-item" key={label}>
+                  <Icon name={icon} hasFill />
+                  <span className="hero-card__tags-label">{label}</span>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
-        <Button className="hero-card__button">
-          <span>View Recipes</span>
-          <Icon name="play" hasFill />
-        </Button>
-        <Image src={badgeImgSrc} />
+        <div className="hero-card__body-extra">
+          <div className="hero-card__author">
+            <Image src={author.imgSrc} />
+            <div className="hero-card__author-body">
+              <p className="hero-card__author-name">{author.name}</p>
+              <time
+                className="hero-card__author-date"
+                dateTime={author.date.dateTime}
+              >
+                {author.date.label}
+              </time>
+            </div>
+          </div>
+          <Button className="hero-card__button">
+            <span>View Recipes</span>
+            <Icon name="play" hasFill />
+          </Button>
+        </div>
+        <Image className="hero-card__badge" src={badgeImgSrc} />
       </div>
       <Image src={bgImgSrc} />
     </article>
