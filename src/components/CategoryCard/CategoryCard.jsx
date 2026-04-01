ab632235d296c93d5219f21434b20171d0a8dee4
Sabyrka <sabyrka23@gmail.com>
2026-04-01 17:27:58 +0500

    feat: enhance CategoryCard component with image and label display

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -1,8 +1,23 @@
 import './CategoryCard.scss'
-import clsx from 'clsx'
+import { Image } from 'minista'
 
 export default (props) => {
-  const { className } = props
+  const { imgSrc, label, color } = props
 
-  return <div className={clsx('category-card', className)}>CategoryCard</div>
+  return (
+    <article className="category-card">
+      <div
+        className="category-card__body"
+        style={{ '--categoryCardColor': color }}
+      >
+        <div
+          className="category-card__image-wrapper"
+          style={{ '--catergodyCardImage': `url('${imgSrc}')` }}
+        >
+          <Image className="category-card__image" src={imgSrc} />
+        </div>
+        <h3 className="category-card__title h6">{label}</h3>
+      </div>
+    </article>
+  )
 }
