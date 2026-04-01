1ab06cecf89f447212c1e5d18f9e072dd3d6b3dc
Sabyrka <sabyrka23@gmail.com>
2026-04-01 21:38:16 +0500

    feat: enhance Banner component with content and button for improved user engagement

--- src/sections/Banner/Banner.jsx
+++ src/sections/Banner/Banner.jsx
@@ -1,8 +1,28 @@
 import './Banner.scss'
-import clsx from 'clsx'
+import Button from '@/components/Button'
+import bannerBg from '@/assets/images/banner/banner-bg.svg'
 
-export default (props) => {
-  const { className } = props
-
-  return <div className={clsx('banner', className)}>Banner</div>
+export default () => {
+  return (
+    <section className="banner">
+      <div className="banner__inner container" aria-labelledby="banner-title">
+        <div className="banner__body">
+          <h2 className="banner__title" id="banner-title">
+            Everyone can be a chef in their own kitchen
+          </h2>
+          <div className="banner__description">
+            <p>
+              Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad
+              minim
+            </p>
+          </div>
+          <Button className="banner__button" href="/">
+            Learn More
+          </Button>
+        </div>
+        <img src={bannerBg} alt="" width="" height="" loading="lazy" />
+      </div>
+    </section>
+  )
 }
