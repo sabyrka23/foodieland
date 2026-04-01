71a8e9fa6998a5b5a6c5b3bfd37e41a26221f171
Sabyrka <sabyrka23@gmail.com>
2026-04-01 23:49:23 +0500

    feat: enhance RecipeCard component to include image, title, and tags

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,8 +1,22 @@
 import './RecipeCard.scss'
 import clsx from 'clsx'
+import Tags from '@/components/Tags'
 
 export default (props) => {
-  const { className } = props
+  const { className, imgSrc, title, tags } = props
 
-  return <div className={clsx('recipe-card', className)}>RecipeCard</div>
+  return (
+    <article className={clsx('recipe-card', className)}>
+      <img
+        className="recipe-card__image"
+        src={imgSrc}
+        alt=""
+        width="368"
+        height="250"
+        loading="lazy"
+      />
+      <h3 className="recipe-card__title">{title}</h3>
+      <Tags className="recipe-card__tags" tags={tags} />
+    </article>
+  )
 }
