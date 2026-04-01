247c457d73be0e87dc54604241cfc71e0e68c920
Sabyrka <sabyrka23@gmail.com>
2026-04-01 20:02:04 +0500

    fix: replace slider implementation in Hero component with Slider component

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,8 +1,6 @@
 import './Hero.scss'
-import 'swiper/css'
-import 'swiper/css/effect-coverflow'
-import { heroCards, sliderConfig } from './hero.data'
-import HeroCard from '@/components/HeroCard'
+import { heroCards } from './hero.data'
+import Slider from '@/components/Slider'
 
 export default () => {
   return (
@@ -10,18 +8,7 @@ export default () => {
       <h1 className="visually-hidden" id="hero-title">
         Special Offers
       </h1>
-      <div
-        className="hero__slider swiper"
-        data-js-slider={JSON.stringify(sliderConfig)}
-      >
-        <ul className="hero__slider-list swiper-wrapper">
-          {heroCards.map(({ id, ...card }) => (
-            <li className="hero__slider-item swiper-slide" key={id}>
-              <HeroCard {...card} />
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Slider class='hero__slider' items={heroCards} />
     </section>
   )
 }
