6969584941b822ea50b62fab04d66cc9043681b2
Sabyrka <sabyrka23@gmail.com>
2026-04-01 12:44:38 +0500

    feat: add Special Offers title and slider to Hero section

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -3,7 +3,22 @@ import './Hero.scss'
 export default () => {
   return (
     <section className="hero" aria-labelledby="hero-title">
-      <div className="hero__inner container">Hero section content</div>
+      <h1 className="hero__title" id="hero-title">
+        Special Offers
+      </h1>
+      <div className="hero__slider swiper" data-js-slider="">
+        <ul className="hero__slider-list swiper-wrapper">
+          <li className="hero__slider-item swiper-slide">
+            <div className="hero-card">Slide 1</div>
+          </li>
+          <li className="hero__slider-item swiper-slide">
+            <div className="hero-card">Slide 2</div>
+          </li>
+          <li className="hero__slider-item swiper-slide">
+            <div className="hero-card">Slide 3</div>
+          </li>
+        </ul>
+      </div>
     </section>
   )
 }
