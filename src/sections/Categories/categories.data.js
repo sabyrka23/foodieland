d11f2b45c4ae7d31bd7d9b5e999bde48783f7377
Sabyrka <sabyrka23@gmail.com>
2026-04-01 17:27:40 +0500

    feat: add categories data with images and labels

---
+++ src/sections/Categories/categories.data.js
@@ -0,0 +1,39 @@
+import breakfast from '@/assets/images/categories/breakfast.png'
+import vegan from '@/assets/images/categories/vegan.png'
+import meat from '@/assets/images/categories/meat.png'
+import dessert from '@/assets/images/categories/dessert.png'
+import lunch from '@/assets/images/categories/lunch.png'
+import chocolate from '@/assets/images/categories/chocolate.png'
+
+export const categories = [
+  {
+    imgSrc: breakfast,
+    label: 'Breakfast',
+    color: 'rgb(112 130 70)',
+  },
+  {
+    imgSrc: vegan,
+    label: 'Vegan',
+    color: 'rgb(108 198 63)',
+  },
+  {
+    imgSrc: meat,
+    label: 'Meat',
+    color: 'rgb(204 38 27)',
+  },
+  {
+    imgSrc: dessert,
+    label: 'Dessert',
+    color: 'rgb(240 158 0)',
+  },
+  {
+    imgSrc: lunch,
+    label: 'Lunch',
+    color: 'rgb(0 0 0)',
+  },
+  {
+    imgSrc: chocolate,
+    label: 'Chocolate',
+    color: 'rgb(0 0 0)',
+  },
+]
