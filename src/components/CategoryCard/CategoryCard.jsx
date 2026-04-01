c552f31cc5d11955e9622e3de109f0ea70e7ab13
Sabyrka <sabyrka23@gmail.com>
2026-04-01 20:07:02 +0500

    fix: add width and height attributes to Image component in CategoryCard

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -14,7 +14,12 @@ export default (props) => {
           className="category-card__image-wrapper"
           style={{ '--catergodyCardImage': `url('${imgSrc}')` }}
         >
-          <Image className="category-card__image" src={imgSrc} />
+          <Image
+            className="category-card__image"
+            src={imgSrc}
+            width={100}
+            height={100}
+          />
         </div>
         <h3 className="category-card__title h6">{label}</h3>
       </div>
