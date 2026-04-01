3e5ab5a421207281bc229aff38f0f367e83b7971
Sabyrka <sabyrka23@gmail.com>
2026-04-01 18:49:19 +0500

    feat: restructure SubscribeForm component for improved layout and accessibility

--- src/sections/SubscribeForm/SubscribeForm.jsx
+++ src/sections/SubscribeForm/SubscribeForm.jsx
@@ -1,39 +1,40 @@
 import './SubscribeForm.scss'
-import clsx from 'clsx'
 import Button from '@/components/Button'
 
-export default (props) => {
-  const { className } = props
-
+export default () => {
   return (
     <section
-      className={clsx('subscribe-form container', className)}
+      className="subscribe-form container"
       aria-labelledby="subscribe-form-title"
     >
-      <h2 className="subscribe-form__title" id="subscribe-form-title">
-        Deliciousness to your inbox
-      </h2>
-      <div className="subscribe-form__description">
-        <p>
-          Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqut enim ad minim
-        </p>
-      </div>
-      <form className="subscribe-form__form">
-        <div className="subscribe-form__field">
-          <label className="visually-hidden" htmlFor="subscribe-form">
-            Email
-          </label>
-          <input
-            className="subscribe-form__input"
-            id="subscribe-form"
-            type="email"
-            placeholder="Your email address..."
-            required
-          />
-          <Button className="subscribe-form__button">Subscribe</Button>
+      <div className="subscribe-form__inner">
+        <h2 className="subscribe-form__title" id="subscribe-form-title">
+          Deliciousness to your inbox
+        </h2>
+        <div className="subscribe-form__description">
+          <p>
+            Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqut enim ad minim
+          </p>
         </div>
-      </form>
+        <form className="subscribe-form__form">
+          <div className="subscribe-form__field">
+            <label className="visually-hidden" htmlFor="subscribe-form">
+              Email
+            </label>
+            <input
+              className="subscribe-form__input"
+              id="subscribe-form"
+              type="email"
+              placeholder="Your email address..."
+              required
+            />
+            <Button className="subscribe-form__button" type="submit">
+              Subscribe
+            </Button>
+          </div>
+        </form>
+      </div>
     </section>
   )
 }
