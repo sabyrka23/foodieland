bf83ded1e4b94587802a6226865e1543a179f1a0
Sabyrka <sabyrka23@gmail.com>
2026-04-01 20:42:39 +0500

    fix: update favicon implementation to support light and dark modes

--- src/global.jsx
+++ src/global.jsx
@@ -3,10 +3,9 @@ import { Head } from 'minista'
 import Header from '@/layouts/Header'
 import Content from '@/layouts/Content'
 import Footer from '@/layouts/Footer'
-import appleTouchIcon from '@/assets/favicons/apple-touch-icon.png'
-import favicon32 from '@/assets/favicons/favicon-32x32.png'
-import favicon16 from '@/assets/favicons/favicon-16x16.png'
-import manifest from '@/assets/favicons/site.webmanifest'
+
+import faviconLight from '@/assets/favicons/favicon-light.svg'
+import faviconDark from '@/assets/favicons/favicon-dark.svg'
 
 export default (props) => {
   const { children, title, url } = props
@@ -16,10 +15,16 @@ export default (props) => {
       <Head htmlAttributes={{ lang: 'en' }}>
         <title>Foodieland | {title}</title>
         <script src="/src/main.js" type="module" />
-        <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
-        <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
-        <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
-        <link rel="manifest" href={manifest} />
+        <link
+          rel="icon"
+          href={faviconLight}
+          media="(prefers-color-scheme: light)"
+        />
+        <link
+          rel="icon"
+          href={faviconDark}
+          media="(prefers-color-scheme: dark)"
+        />
       </Head>
       <Header url={url} />
       <Content>{children}</Content>
