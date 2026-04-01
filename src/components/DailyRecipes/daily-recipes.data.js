17594040e30d6307c08fb9a8b13f6fd760174568
Sabyrka <sabyrka23@gmail.com>
2026-04-02 01:50:45 +0500

    feat: add daily recipes data for burger dishes

---
+++ src/components/DailyRecipes/daily-recipes.data.js
@@ -0,0 +1,124 @@
+import burgerImage from '@/assets/images/recipes/burger.jpg'
+
+export const dailyRecipes = [
+  {
+    id: 1,
+    imgSrc: burgerImage,
+    title: 'Big and Juicy Wagyu Beef Cheeseburger',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Snack',
+      },
+    ],
+  },
+  {
+    id: 2,
+    imgSrc: burgerImage,
+    title: 'Big and Juicy Wagyu Beef Cheeseburger',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Snack',
+      },
+    ],
+  },
+  {
+    id: 3,
+    imgSrc: burgerImage,
+    title: 'Big and Juicy Wagyu Beef Cheeseburger',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Snack',
+      },
+    ],
+  },
+  {
+    id: 4,
+    imgSrc: burgerImage,
+    title: 'Big and Juicy Wagyu Beef Cheeseburger',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Snack',
+      },
+    ],
+  },
+  {
+    id: 5,
+    imgSrc: burgerImage,
+    title: 'Big and Juicy Wagyu Beef Cheeseburger',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Snack',
+      },
+    ],
+  },
+  {
+    id: 6,
+    imgSrc: burgerImage,
+    title: 'Big and Juicy Wagyu Beef Cheeseburger',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Snack',
+      },
+    ],
+  },
+  {
+    id: 7,
+    imgSrc: burgerImage,
+    title: 'Big and Juicy Wagyu Beef Cheeseburger',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Snack',
+      },
+    ],
+  },
+  {
+    id: 8,
+    imgSrc: burgerImage,
+    title: 'Big and Juicy Wagyu Beef Cheeseburger',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Snack',
+      },
+    ],
+  },
+]
