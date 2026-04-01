b8b473ab6ab83cf282c041f778a7422e55746088
Sabyrka <sabyrka23@gmail.com>
2026-04-01 23:49:10 +0500

    feat: enhance Tags component to display a list of tags with icons

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -1,8 +1,20 @@
 import './Tags.scss'
 import clsx from 'clsx'
+import Icon from '@/components/Icon'
 
 export default (props) => {
-  const { className } = props
+  const { className, tags } = props
 
-  return <div className={clsx('tags', className)}>Tags</div>
+  return (
+    <div className={clsx('tags', className)}>
+      <ul className="tags__list">
+        {tags.map(({ icon, label }) => (
+          <li className="tags__item" key={label}>
+            <Icon name={icon} hasFill />
+            <span className="tags__label">{label}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
