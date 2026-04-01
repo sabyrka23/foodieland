1d9b41f1aa425934918e7a74e0911f020160368f
Sabyrka <sabyrka23@gmail.com>
2026-04-02 01:27:45 +0500

    feat: enhance InstagramView component with layout and functionality

--- src/sections/InstagramView/InstagramView.jsx
+++ src/sections/InstagramView/InstagramView.jsx
@@ -1,8 +1,37 @@
 import './InstagramView.scss'
-import clsx from 'clsx'
+import Section from '@/layouts/Section'
+import Grid from '@/components/Grid'
+import Button from '@/components/Button'
+import Icon from '@/components/Icon'
+import instagramPostImage from '@/assets/images/instagram-view/instagram-post.jpg'
 
-export default (props) => {
-  const { className } = props
-
-  return <div className={clsx('instagram-view', className)}>InstagramView</div>
+export default () => {
+  return (
+    <div className="instagram-view">
+      <Section
+        title="Check out @foodieland on Instagram"
+        titleId="instagram-view"
+        description="Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim"
+        headerMode="column"
+      >
+        <Grid columns={4}>
+          {[...Array(4)].map((_, index) => (
+            <img
+              className="instagram-view__image"
+              src={instagramPostImage}
+              alt=""
+              width={290}
+              height={446}
+              loading="lazy"
+              key={index}
+            />
+          ))}
+        </Grid>
+      </Section>
+      <Button className="instagram-view__button">
+        <span>Visit Our Instagram</span>
+        <Icon name="instagram" hasFill />
+      </Button>
+    </div>
+  )
 }
