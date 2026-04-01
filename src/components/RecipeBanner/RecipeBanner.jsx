0a27c0490f14c7ac07314ccf42744384b3e7f816
Sabyrka <sabyrka23@gmail.com>
2026-04-02 00:29:06 +0500

    feat: add images and link to RecipeBanner component for enhanced presentation

--- src/components/RecipeBanner/RecipeBanner.jsx
+++ src/components/RecipeBanner/RecipeBanner.jsx
@@ -1,8 +1,18 @@
 import './RecipeBanner.scss'
 import clsx from 'clsx'
+import titleImage from '@/assets/images/recipe-banner/recipe-banner-title.svg'
+import dishImage from '@/assets/images/recipe-banner/recipe-banner-dish.png'
 
 export default (props) => {
   const { className } = props
 
-  return <div className={clsx('recipe-banner', className)}>RecipeBanner</div>
+  return (
+    <div className={clsx('recipe-banner', className)}>
+      <img className="recipe-banner__title" src={titleImage} alt="" />
+      <img className="recipe-banner__dish" src={dishImage} alt="" />
+      <a className="recipe-banner__link" href="/">
+        www.foodieland.com
+      </a>
+    </div>
+  )
 }
