ba785baa85f0a5d7fa7a54465232519b9226950d
Sabyrka <sabyrka23@gmail.com>
2026-04-01 13:18:25 +0500

    feat: add slider configuration and integrate into Hero component

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,12 +1,16 @@
 import './Hero.scss'
+import { sliderConfig } from './hero.data'
 
 export default () => {
   return (
     <section className="hero" aria-labelledby="hero-title">
-      <h1 className="hero__title" id="hero-title">
+      <h1 className="visually-hidden" id="hero-title">
         Special Offers
       </h1>
-      <div className="hero__slider swiper" data-js-slider="">
+      <div
+        className="hero__slider swiper"
+        data-js-slider={JSON.stringify(sliderConfig)}
+      >
         <ul className="hero__slider-list swiper-wrapper">
           <li className="hero__slider-item swiper-slide">
             <div className="hero-card">Slide 1</div>
---
+++ src/sections/Hero/hero.data.js
@@ -0,0 +1,5 @@
+export const sliderConfig = {
+  spaceBetween: 40,
+  slidesPerView: 1.0625,
+  centeredSlides: true,
+}
