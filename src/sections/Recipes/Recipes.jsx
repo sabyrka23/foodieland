ba36bc8e37d38d5625e6c7ef526c979ba41b127c
Sabyrka <sabyrka23@gmail.com>
2026-04-01 23:49:58 +0500

    feat: implement recipe listing with RecipeCard and RecipeBanner components

--- src/sections/Recipes/Recipes.jsx
+++ src/sections/Recipes/Recipes.jsx
@@ -1,6 +1,9 @@
 import './Recipes.scss'
 import Section from '@/layouts/Section'
 import Grid from '@/components/Grid'
+import { recipes } from './recipes.data'
+import RecipeBanner from '@/components/RecipeBanner'
+import RecipeCard from '@/components/RecipeCard'
 
 export default () => {
   return (
@@ -9,7 +12,15 @@ export default () => {
       titleId="recipes"
       description="Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim "
     >
-      <Grid columns={3}></Grid>
+      <Grid columns={3}>
+        {recipes.map(({ type, id, ...rest }) =>
+          type === 'recipe' ? (
+            <RecipeCard key={id} {...rest} />
+          ) : (
+            <RecipeBanner key={id} />
+          )
+        )}
+      </Grid>
     </Section>
   )
 }
