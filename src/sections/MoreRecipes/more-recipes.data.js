41e1727aa13e45daf0e773e4936bff6ab2ef1396
Sabyrka <sabyrka23@gmail.com>
2026-04-02 11:35:25 +0500

    feat: add more recipes data for burger items

---
+++ src/sections/MoreRecipes/more-recipes.data.js
@@ -0,0 +1,70 @@
+import burgerImage from '@/assets/images/recipes/burger.jpg'
+import likeIcon from '@/assets/icons/like/like.svg'
+import unlikeIcon from '@/assets/icons/like/unlike.svg'
+
+export const moreRecipes = [
+  {
+    id: 1,
+    imgSrc: burgerImage,
+    title: 'Big and Juicy Wagyu Beef Cheeseburger',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Snack',
+      },
+    ],
+    isLiked: likeIcon,
+  },
+  {
+    id: 2,
+    imgSrc: burgerImage,
+    title: 'Big and Juicy Wagyu Beef Cheeseburger',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Snack',
+      },
+    ],
+    isLiked: unlikeIcon,
+  },
+  {
+    id: 3,
+    imgSrc: burgerImage,
+    title: 'Big and Juicy Wagyu Beef Cheeseburger',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Snack',
+      },
+    ],
+    isLiked: likeIcon,
+  },
+  {
+    id: 4,
+    imgSrc: burgerImage,
+    title: 'Big and Juicy Wagyu Beef Cheeseburger',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Snack',
+      },
+    ],
+    isLiked: unlikeIcon,
+  },
+]
