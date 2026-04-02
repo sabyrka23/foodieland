4ee9bcb92630c886ee92bbff39b996ff8d96d1d4
Sabyrka <sabyrka23@gmail.com>
2026-04-02 08:28:39 +0500

    feat: add Blog component with SubscribeForm integration

---
+++ src/pages/blog.jsx
@@ -0,0 +1,13 @@
+import SubscribeForm from '../sections/SubscribeForm'
+
+export const metadata = {
+  title: 'Blog',
+}
+
+export default () => {
+  return (
+    <>
+      <SubscribeForm />
+    </>
+  )
+}
