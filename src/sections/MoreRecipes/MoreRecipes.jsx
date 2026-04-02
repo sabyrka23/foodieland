02f15a12395b86c8882ea41e695c1b30fe7ef8da
Sabyrka <sabyrka23@gmail.com>
2026-04-02 11:35:29 +0500

    feat: implement MoreRecipes component with grid layout and section header

--- src/sections/MoreRecipes/MoreRecipes.jsx
+++ src/sections/MoreRecipes/MoreRecipes.jsx
@@ -1,8 +1,21 @@
 import './MoreRecipes.scss'
-import clsx from 'clsx'
+import Grid from '@/components/Grid'
+import RecipeCard from '@/components/RecipeCard'
+import Section from '@/layouts/Section'
+import { moreRecipes } from './more-recipes.data'
 
-export default (props) => {
-  const { className } = props
-
-  return <div className={clsx('more-recipes', className)}>MoreRecipes</div>
+export default () => {
+  return (
+    <Section
+      title="You may like these recipe too"
+      titleId="more-recipes"
+      headerMode="center"
+    >
+      <Grid columns={4}>
+        {moreRecipes.map(({ id, ...recipe }) => (
+          <RecipeCard titleTag="h6" key={id} {...recipe} />
+        ))}
+      </Grid>
+    </Section>
+  )
 }
