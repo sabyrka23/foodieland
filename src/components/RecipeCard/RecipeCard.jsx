22e1d01c9987687d2a7e2bd8e2560c152a3f9ba6
Sabyrka <sabyrka23@gmail.com>
2026-04-02 10:52:46 +0500

    feat: add like button functionality to RecipeCard component

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -3,17 +3,30 @@ import clsx from 'clsx'
 import Tags from '@/components/Tags'
 
 export default (props) => {
-  const { mode, imgSrc, title, titleTag, tags } = props
+  const { mode, imgSrc, title, titleTag, tags, isLiked } = props
 
   return (
     <article className={`recipe-card recipe-card--${mode}`}>
       <a className={clsx('recipe-card__inner')} href="/">
-        <img
-          className="recipe-card__image"
-          src={imgSrc}
-          alt=""
-          loading="lazy"
-        />
+        <span className="recipe-card__image-wrapper">
+          {isLiked && (
+            <button
+              className="recipe-card__button"
+              title={isLiked ? 'Unlike' : 'Like'}
+            >
+              <span className="visually-hidden">
+                {isLiked ? 'Unlike' : 'Like'}
+              </span>
+              <img src={isLiked} alt="" width={24} height={24} loading="lazy" />
+            </button>
+          )}
+          <img
+            className="recipe-card__image"
+            src={imgSrc}
+            alt=""
+            loading="lazy"
+          />
+        </span>
         <h3 className={clsx('recipe-card__title', titleTag)}>{title}</h3>
         <Tags className="recipe-card__tags" tags={tags} />
       </a>
